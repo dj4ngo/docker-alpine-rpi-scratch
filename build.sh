20bbeebc28619c24842e15bf0ca7e38dfc4dbc5f
Sebastien Boyron <sebastien@boyron.eu>
2017-08-22 01:30:43 +0200

    first fully working version on x86_64

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -e
 
-# Test root user
-if [ "$(id -u)" -ne 0 ]; then 
-	echo "$0 needs to be runned as root ! " >&2
-	exit 100
-fi 
+#TODO : add keys for apk
+
 
 ### ENV VARS ###
-TMP_DIR="/tmp/alpine-tmp-$$"
-TMP_ROOTFS="/tmp/alpine-rootfs-$$"
+#TMP_DIR="/tmp/alpine-tmp-$$"
+TMP_DIR="alpine-tmp"
+#TMP_ROOTFS="/tmp/alpine-rootfs-$$"
+TMP_ROOTFS="alpine-rootfs"
 MIRROR="http://nl.alpinelinux.org/alpine"
 RELEASE="latest-stable"
 REPO="${MIRROR}/${RELEASE}/main"
@@ -16,37 +15,121 @@ ARCH="armhf"
 TAG="dj4ngo/alpine-rpi"
 DOCKER_BUILD="dockerBuild"
 
-### MAIN ###
-
-echo "Prepare build env"
-echo "  create ${TMP_DIR} ${TMP_ROOTFS}"
-mkdir -p ${TMP_DIR}
-mkdir -p ${TMP_ROOTFS}
-mkdir -p ${DOCKER_BUILD}
-# clean
-trap "rm -rf ${TMP_DIR} ${TMP_ROOTFS}" EXIT TERM INT
-
-apk_version=$(curl -s $REPO/$(uname -m)/APKINDEX.tar.gz | tar -Oxz | sed -n '/apk-tools-static/{n;s/V://p}')
-echo "  APK version=$apk_version"
-curl -s ${REPO}/$(uname -m)/apk-tools-static-${apk_version}.apk | tar -xz -C $TMP_DIR sbin/apk.static 2>/dev/null
-
-echo "Create Base rootfs"
-${TMP_DIR}/sbin/apk.static -v --arch $ARCH --repository $REPO --update-cache --root $TMP_ROOTFS --initdb add alpine-base --allow-untrusted --purge --no-progress
-
-ls -l $TMP_ROOTFS/bin
-echo "Configure repository"
-echo "$REPO" > $TMP_ROOTFS/etc/apk/repositories
 
 
-echo "Create alpine-rootfs.tgz"
-# Check if runned only to generate Docker build environment
-if [ "$1" == "genDocker" ]; then 
-	tar --numeric-owner -C $TMP_ROOTFS -zcvf ${DOCKER_BUILD}/rootfs.tgz .
-else
-	echo "Import in Dockerfile"
+function test_root_user () {
+	echo "-> Test root user"
+	if [ "$(id -u)" -ne 0 ]; then 
+		echo "$0 needs to be runned as root ! " >&2
+		exit 100
+	fi 
+}
+
+
+function create_arbo () {
+	echo "-> Prepare build env"
+	echo "---> Create ${TMP_DIR} ${TMP_ROOTFS}" 
+	mkdir -p ${TMP_DIR}
+	mkdir -p ${TMP_ROOTFS}
+	mkdir -p ${DOCKER_BUILD}
+	# clean
+	trap "rm -rf ${TMP_DIR} ${TMP_ROOTFS}" EXIT TERM INT
+}
+
+function get_apk_static () {
+	echo "-> Install apk.static for $(uname -m)"
+	apk_version=$(curl -s $REPO/$(uname -m)/APKINDEX.tar.gz | tar -Oxz | sed -n '/apk-tools-static/{n;s/V://p}')
+	echo "---> APK version=$apk_version"
+	curl -s ${REPO}/$(uname -m)/apk-tools-static-${apk_version}.apk | tar -xz -C $TMP_DIR sbin/apk.static 2>/dev/null
+}
+
+function compile_resin-xbuild () {
+	echo "-> Compile resin-xbuild"
+	echo "---> Get resin-xbuild from https://github.com/resin-io-projects/armv7hf-debian-qemu"
+	mkdir -p ${TMP_DIR}/resin-xbuild
+	pushd ${TMP_DIR}/resin-xbuild
+	curl "https://raw.githubusercontent.com/resin-io-projects/armv7hf-debian-qemu/master/resin-xbuild.go" -o resin-xbuild.go
+	echo "---> Compile resin-xbuild"
+	go build -ldflags "-w -s" resin-xbuild.go
+	popd
+
+}
+
+function install_resin-xbuild () {
+
+	echo "-> Install resin-xbuild"
+
+	echo "---> Create /usr/bin and /bin"
+	mkdir -p $TMP_ROOTFS/{usr/,}bin
+
+	echo "---> Copy resin-xbuild binary"
+	cp ${TMP_DIR}/resin-xbuild/resin-xbuild $TMP_ROOTFS/usr/bin
+
+	echo "---> Create sh.real"
+	ln -s /bin/busybox $TMP_ROOTFS/bin/sh
+
+	echo "---> Create crossbuild start and end links"
+	
+	ln -s resin-xbuild $TMP_ROOTFS/usr/bin/cross-build-end
+	ln -s resin-xbuild $TMP_ROOTFS/usr/bin/cross-build-start
+
+
+	echo "---> Create sh and sh-shim"
+	cat <<EOF | tee $TMP_ROOTFS/bin/sh > $TMP_ROOTFS/bin/sh-shim
+#!/usr/bin/qemu-arm-static /bin/sh.real
+
+set -o errexit
+
+cp /bin/sh.real /bin/sh  
+/bin/sh "$@"
+cp /usr/bin/sh-shim /bin/sh
+EOF
+	echo "---> Copy qemu-arm-static"
+	cp $(which qemu-arm-static) $TMP_ROOTFS/usr/bin/
+
+	echo "---> Set erveything executable"
+	chmod +x $TMP_ROOTFS/bin/sh* $TMP_ROOTFS/usr/bin/*
+}
+
+function install_rootfs () {
+
+	chroot $TMP_ROOTFS cross-build-start
+	${TMP_DIR}/sbin/apk.static -v --arch $ARCH --repository $REPO --update-cache --root $TMP_ROOTFS --initdb add alpine-base --allow-untrusted --purge --no-progress
+	chroot $TMP_ROOTFS cross-build-end
+}
+
+function configure_repository () {
+
+	echo "-> Configure repository"
+	echo "$REPO" > $TMP_ROOTFS/etc/apk/repositories
+
+}
+
+function generate_tgz () {
+	echo "-> Create alpine rootfs.tgz"
+	tar --numeric-owner -C $TMP_ROOTFS -zcf ${DOCKER_BUILD}/rootfs.tgz .
+}
+
+
+function import_in_docker () {
+
+	echo "-> Import in Dockerfile"
 	image_id=$(tar --numeric-owner -C $TMP_ROOTFS -c .  | docker import - ${TAG}:${RELEASE})
 	docker tag $image_id ${TAG}:latest
 	echo "Alpine imported : ${TAG}:latest"
+}
+### MAIN ###
+
+test_root_user
+create_arbo
+get_apk_static
+compile_resin-xbuild
+install_resin-xbuild
+install_rootfs
+if [ "$1" == "import" ]; then 
+	import_in_docker
+else
+	generate_tgz
 fi
 
 
