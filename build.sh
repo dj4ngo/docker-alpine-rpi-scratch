a044865aea7a50b1bf0439f4c43a4f385c2d9c34
Sebastien Boyron <sboyron.ext@orange.com>
2017-08-23 13:42:19 +0200

    try new build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 #TODO : add keys for apk
 
@@ -18,54 +18,74 @@ export GOTOOLDIR="/usr/lib/go/pkg/tool/linux_amd64"
 
 function usage () {
 	cat << EOF
-
+USAGE : ${0} <function_name>
+	list of available <function_name> :
+EOF
+	for func in $available_functions; do
+		echo -e "\t- $func"
+	done
+	cat << EOF
+	If you want to build localy the Docker Container and add it to your local docker, just run :
+	${0} local_build
 EOF
 }
 
 
 function test_root_user () {
+	
 	echo "-> Test root user"
 	if [ "$(id -u)" -ne 0 ]; then 
 		echo "$0 needs to be runned as root ! " >&2
 		exit 100
 	fi 
+	
 }
 
 function install_dep () {
+	
 	echo "-> Install dependencies for build"
 	apt-get update -qq
 	apt-get install -y  curl golang qemu-user-static
+	
 }
 
 function create_arbo () {
+	
 	echo "-> Prepare build env"
 	echo "---> Create ${TMP_DIR} ${TMP_ROOTFS}" 
 	mkdir -p ${TMP_DIR}
 	mkdir -p ${TMP_ROOTFS}
 	mkdir -p ${DOCKER_BUILD}
+	cp Dockerfile ${DOCKER_BUILD}/
+	
 }
 
 function get_apk_static () {
+	
 	echo "-> Install apk.static for $(uname -m)"
 	apk_version=$(curl -s $REPO/$(uname -m)/APKINDEX.tar.gz | tar -Oxz | sed -n '/apk-tools-static/{n;s/V://p}')
 	echo "---> APK version=$apk_version"
-	curl -s ${REPO}/$(uname -m)/apk-tools-static-${apk_version}.apk | tar -xz -C $TMP_DIR sbin/apk.static 2>/dev/null
+	curl -s ${REPO}/$(uname -m)/apk-tools-static-${apk_version}.apk | tar -xz -C $TMP_DIR sbin/apk.static 
+	
 }
 
-function compile_resin-xbuild () {
-	echo "-> Compile resin-xbuild"
+function get_resin-xbuild () {
 	echo "---> Get resin-xbuild from https://github.com/resin-io-projects/armv7hf-debian-qemu"
 	mkdir -p ${TMP_DIR}/resin-xbuild
 	pushd ${TMP_DIR}/resin-xbuild
 	curl "https://raw.githubusercontent.com/resin-io-projects/armv7hf-debian-qemu/master/resin-xbuild.go" -o resin-xbuild.go
-	echo "---> Compile resin-xbuild"
+	popd
+}
 
+function compile_resin-xbuild () {
+	echo "-> Compile resin-xbuild"
+	pushd ${TMP_DIR}/resin-xbuild
 	go build -ldflags "-w -s" resin-xbuild.go
 	popd
 }
 
 function install_resin-xbuild () {
-
+	
 	echo "-> Install resin-xbuild"
 	echo "---> Create /usr/bin and /bin"
 	mkdir -p $TMP_ROOTFS/{usr/,}bin
@@ -97,34 +117,36 @@ EOF
 
 	echo "---> Set erveything executable"
 	chmod +x $TMP_ROOTFS/bin/sh* $TMP_ROOTFS/usr/bin/*
+	
 }
 
 function install_rootfs () {
-
+	
+	echo "-> Install root FS"
 	chroot $TMP_ROOTFS cross-build-start
 	${TMP_DIR}/sbin/apk.static -v --arch $ARCH --repository $REPO --update-cache --root $TMP_ROOTFS --initdb add alpine-base --allow-untrusted --purge --no-progress
 	chroot $TMP_ROOTFS cross-build-end
-}
-
-function configure_repository () {
-
 	echo "-> Configure repository"
 	echo "$REPO" > $TMP_ROOTFS/etc/apk/repositories
+	
 
 }
 
-function generate_tgz () {
+function generate_rootfstgz () {
+	
 	echo "-> Create alpine rootfs.tgz"
 	tar --numeric-owner -C $TMP_ROOTFS -zcf ${DOCKER_BUILD}/rootfs.tgz .
+
 }
 
 
 function import_in_docker () {
-
+	
 	echo "-> Import in Dockerfile"
 	image_id=$(tar --numeric-owner -C $TMP_ROOTFS -c .  | docker import - ${TAG}:${RELEASE})
 	docker tag $image_id ${TAG}:latest
 	echo "Alpine imported : ${TAG}:latest"
+	
 }
 
 
@@ -145,44 +167,33 @@ function mr_proper () {
 }
 
 ### MAIN ###
+function local_build () {
+	install_dep
+	create_arbo
+	get_apk_static
+	get_resin-xbuild
+	compile_resin-xbuild
+	install_resin-xbuild
+	install_rootfs
+	mr_proper
+}
+
 
 # clean
+mr_proper
 
-test_root_user
+available_functions="$(typeset -F | sed -n 's/^declare -f \([^_].*\)/\1/p')"
+func_name="$1"
+if [ -z "$func_name" ]; then usage; fi
+shift
+args="${@}"
 
-case $1 in
-	localbuild)
-		install_dep
-		create_arbo
-		get_apk_static
-		compile_resin-xbuild
-		install_resin-xbuild
-		install_rootfs
-		mr_proper
-	;;
-	prepare)
-		install_dep
-		create_arbo
-		get_apk_static
-	;;
-	build)
-		compile_resin-xbuild
-		install_resin-xbuild
-		install_rootfs
-	;;
-	test)
-		test_docker_build
-		test_docker_use_img
-	;;
-	deploy)
-		deploy_on_dockerhub
-	;;
-	clean)
-		mrproper
-	;;
-	*)
-		usage
-	;;
-esac
-
-
+if grep -q "\<$func_name\>" <<< $available_functions; then
+	test_root_user
+	echo ">>>>>> RUN $func_name"
+	set -x
+	$func_name $args	
+	set +x
+else
+	usage
+fi
