0065a7e02a2b74545e8198d09f89f2691d4ee000
Sebastien Boyron <sboyron.ext@orange.com>
2017-08-23 18:29:00 +0200

    issue in Dockerfile test use img

--- build.sh
+++ build.sh
@@ -169,7 +169,7 @@ function test_docker_use_img () {
 	cat <<EOF > $BUILD_PATH/Dockerfile-docker_use_img-test
 FROM ${TAG}-test
 RUN cross-build-start \
-    apk update
+    apk update \
     apk add --update python \
     cross-build-end
 CMD ["python", "-c",'print(\"WORKING !!!\"' ]
