c7bccc407b43d48a6eaba01aff8f773d733bac34
Sebastien Boyron <sebastien@boyron.eu>
2017-09-05 08:34:23 +0200

    pending job

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM scratch
+
 LABEL maintainer="sebastien@boyron.eu"
+
 COPY root /
-#ADD http://nl.alpinelinux.org/alpine/latest-stable/main/armhf/apk-tools-static-2.7.2-r0.apk /usr/bin
+
 RUN install.sh
-#RUN /usr/bin/qemu-arm-static /sbin/apk.static -v --arch armhf --repository http://nl.alpinelinux.org/alpine/latest-stable/main  --update-cache --root / --initdb add alpine-base ca-certificates --allow-untrusted --purge --no-progress
 
--- root/bin/install.sh
+++ root/bin/install.sh
@@ -2,14 +2,21 @@
 
 set -x 
 
-mv /bin/sh /bin/sh.orig
-cat << EOF >/bin/sh
+mv /bin/sh /bin/sh.orig >&2
+cat << 'EOF' >/bin/sh
 #!/bin/sh
 
 set -x
-qemu-arm-static ${@}
+case "$(uname -m)" in )
+	"x86_64")
+		qemu-arm-static ${@}
+		;;
+	"arm")
+		${@}
+		;;
+esac
 EOF
+chmod +x /bin/sh
 
-qemu-arm-static apk.static -v --arch armhf --repository http://nl.alpinelinux.org/alpine/latest-stable/main  --update-cache --root / --initdb add alpine-base ca-certificates --allow-untrusted --purge --no-progress
+qemu-arm-static apk.static -v --arch armhf --repository http://nl.alpinelinux.org/alpine/latest-stable/main  --update-cache --root / --initdb add alpine-base ca-certificates --allow-untrusted --purge --no-progress >&2
 
-mv /bin/sh.orig /bin/sh
