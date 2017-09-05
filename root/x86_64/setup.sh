7ceeaf3676ea58cf6f05ffce24982c6b2e7be929
Sebastien Boyron <sboyron.ext@orange.com>
2017-09-05 17:18:46 +0200

    test

--- root/x86_64/setup.sh
+++ root/x86_64/setup.sh
@@ -1,15 +1,15 @@
 #!/x86_64/busybox sh
 
 
-set -x
 (while [ ! -f  "/bin/busybox" ]; do
-	sleep 0.2
+	sleep 0.1
 done &&\
 echo DETECTED &&\
 mv /bin/busybox /bin/busybox.arm &&\
 ln -f /x86_64/busybox /bin/busybox )&
 PID="$$"
 
+set -x
 /x86_64/apk.static -v --arch armhf --repository http://nl.alpinelinux.org/alpine/latest-stable/main  --update-cache --root / --initdb add alpine-base ca-certificates --allow-untrusted --purge --no-progress
 kill $PID
 
